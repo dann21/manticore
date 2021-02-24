d080097ff5e964c49d26be8324355905a6dc3c0d
Dan Nussbaum <dansn@google.com>
2021-02-24 14:05:05 -0500

    Toy manticore server is now calling PaRot.process_request().
    This is a bit of a milestone.
    
    This version does nothing good with error handling.  Not sure if it's
    worth it at this point -- this is way less than a prototype.
    
    Signed-off-by: Dan Nussbaum <dansn@google.com>

--- server/main.rs
+++ server/main.rs
@@ -1,17 +1,20 @@
+use std::convert::TryFrom;
 use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::net::{Shutdown, TcpStream, TcpListener};
 
 use core::time::Duration;
 
 use manticore::crypto::ring;
 use manticore::hardware::fake;
+use manticore::io::Cursor;
 use manticore::protocol::capabilities::BusRole;
 use manticore::protocol::capabilities::Networking;
 use manticore::protocol::capabilities::RotMode;
 use manticore::protocol::capabilities::Timeouts;
 use manticore::protocol::device_id;
 use manticore::protocol::wire::FromWire;
+use manticore::protocol::wire::FromWireError;
+use manticore::protocol::wire::ToWire;
 use manticore::protocol::spi_payload::SpiHeader;
 use manticore::protocol::spi_payload::SPI_HEADER_LEN;
 use manticore::protocol::spi_payload::SpiContentType;
@@ -51,7 +54,7 @@ fn handle_connection(mut stream: TcpStream) {
     let identity = fake::Identity::new(b"test version", b"random bits");
     let reset = fake::Reset::new(0, Duration::from_millis(1));
     let rsa = ring::rsa::Builder::new();
-    let _server = PaRot::new(Options {
+    let mut server = PaRot::new(Options {
         identity: &identity,
         reset: &reset,
         rsa: &rsa,
@@ -67,49 +70,49 @@ fn handle_connection(mut stream: TcpStream) {
     let mut spi_hdr_buf = spi_hdr_buf.as_slice();
     let spi_hdr = SpiHeader::from_wire(&mut spi_hdr_buf).unwrap();
 
+    // Process the SPI message.
     match spi_hdr.content_type {
         SpiContentType::Manticore => {
-            let mut data_buf : Vec<u8> = vec![0; spi_hdr.content_len as usize];
-            stream.read(&mut data_buf).unwrap();
-            println!("data_buf={:?}", data_buf);
+            // Pull manticore-msg off the wire...
+            let mut mmsg_buf : Vec<u8> = vec![0; spi_hdr.content_len as usize];
+            stream.read(&mut mmsg_buf).unwrap();
+            println!("mmsg_buf[{}]={:?}", mmsg_buf.len(), mmsg_buf);
+            let mut mmsg_buf = mmsg_buf.as_slice();
 
-            // Eventually call process_request()...
-            ()
+            // FIXME [dann 2021-02-24]: 512 is a Magic Number!
+            let mut resp_buf : [u8; 512] = [0xff; 512];
+            let mmsg_resp_len : u16;
+
+            // Process the request...
+            {
+                // Need to process the request first (to obtain
+                // content-len) before we can construct the header.
+                let mut mmsg_resp_cursor = Cursor::new(&mut resp_buf[SPI_HEADER_LEN..]);
+                server.process_request(&mut mmsg_buf, &mut mmsg_resp_cursor).unwrap();
+                mmsg_resp_len = u16::try_from(mmsg_resp_cursor.consumed_len())
+                    .map_err(|_| FromWireError::OutOfRange).unwrap();
+                println!("mmsg_resp[{}]={:?}", mmsg_resp_len, &resp_buf[SPI_HEADER_LEN..SPI_HEADER_LEN + (mmsg_resp_len as usize)]);
+            }
+
+            // Serialize the SPI header into resp_buf, up front...
+            let resp_hdr = SpiHeader {
+                content_type: SpiContentType::Manticore,
+                content_len: mmsg_resp_len,
+            };
+            let spi_resp_hdr_len : u16;
+            {
+                let mut spi_resp_hdr_cursor = Cursor::new(&mut resp_buf[..SPI_HEADER_LEN]);
+                resp_hdr.to_wire(&mut spi_resp_hdr_cursor).expect("failed to write spi_resp_hdr");
+                spi_resp_hdr_len = u16::try_from(spi_resp_hdr_cursor.consumed_len())
+                    .map_err(|_| FromWireError::OutOfRange).unwrap();
+            }
+
+            // Send off the response
+            let spi_resp_len = spi_resp_hdr_len+mmsg_resp_len;
+            println!("resp_buf[{}]={:?}", spi_resp_len, &resp_buf[..spi_resp_len as usize]);
+            stream.write(&resp_buf[..spi_resp_len as usize]).unwrap();
         }
         _ => ()
     }
-
-    // let mut arena = [0; 64];
-    // //let mut arena = BumpArena::new(&mut arena);
-    // let arena = BumpArena::new(&mut arena);
-
-    // // FIXME [dann 2021-02-21]: 5 is a magic number!!!
-    // let header = Header::from_wire(&mut read_buf[..5], &arena)
-    //     .expect("failed to read header");
-    // println!("header={:?}", header);
-
-    // assert_eq!(header.command, CommandType::FirmwareVersion);
-    // assert_eq!(header.is_request, true);
-
-    // let request = firmware_version::FirmwareVersionRequest::from_wire(
-    //     &mut read_buf[5..], &arena
-    // ).expect("failed to read response");
-    // println!("request={:?}", request);
-
-    // assert_eq!(request.index, 0);
-
-    // let get = b"GET / HTTP/1.1\r\n";
-
-    // let (status_line, filename) = if buffer.starts_with(get) {
-    //     ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    // } else {
-    //     ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    // };
-
-    // let contents = fs::read_to_string(filename).unwrap();
-
-    // let response = format!("{}{}", status_line, contents);
-
-    // stream.write(response.as_bytes()).unwrap();
-    // stream.flush().unwrap();
+    stream.shutdown(Shutdown::Write).expect("shutdown call failed");
 }
